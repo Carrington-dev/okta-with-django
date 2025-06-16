e0deb8327ff876a95564fe7b51b1ea9895120756
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-06-16 02:18:08 +0200

    Update settings.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington <215159144+carrington-cs@users.noreply.github.com>

--- oktaweb/settings.py
+++ oktaweb/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
+    'corsheaders',
     'security',
 ]
 
